899c5dbabb64f03d1aefba30c33abd68d2c166ec
Dan Aument <danaument@gmail.com>
2020-11-30 13:52:52 -0600

    reworked input processing

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,73 +4,62 @@ var date = "20201125";
 var lat = "38.99";
 var lon = "-75.51";
 var stateInput;
-
+var currentTrip;
+var searchHistory;
 var stateDatabank = {
-    Alabama: { abbr: "AL", lat: "32.77", lon: "-86.83" },
-    Alaska: { abbr: "AK", lat: "64.07", lon: "-152.28" },
-    Arizona: { abbr: "AZ", lat: "34.27", lon: "-111.66" },
-    Arkansas: { abbr: "AR", lat: "34.89", lon: "-92.44" },
-    California: { abbr: "CA", lat: "37.18", lon: "-119.47" },
-    Colorado: { abbr: "CO", lat: "38.99", lon: "-105.55" },
-    Connencticut: { abbr: "CT", lat: "41.62", lon: "-105.55" },
-    Delaware: { abbr: "DE", lat: "38.99", lon: "-75.51" },
-    DC: { abbr: "DC", lat: "38.91", lon: "-77.01" },
-    Florida: { abbr: "FL", lat: "28.68", lon: "-82.46" },
-    Georgia: { abbr: "GA", lat: "32.66", lon: "-83.43" },
-    Hawaii: { abbr: "HI", lat: "20.29", lon: "-156.37" },
-    Idaho: { abbr: "ID", lat: "44.35", lon: "-114.61" },
-    Illinois: { abbr: "IL", lat: "40.04", lon: "-89.19" },
-    Indiana: { abbr: "IN", lat: "39.89", lon: "-86.28" },
-    Iowa: { abbr: "IA", lat: "42.07", lon: "-93.49" },
-    Kansas: { abbr: "KS", lat: "38.49", lon: "-98.38" },
-    Kentucky: { abbr: "KY", lat: "37.53", lon: "-85.30" },
-    Louisiana: { abbr: "LA", lat: "31.06", lon: "-91.99" },
-    Maine: { abbr: "ME", lat: "45.36", lon: "-69.24" },
-    Maryland: { abbr: "MD", lat: "39.05", lon: "-76.79" },
-    Massachusetts: { abbr: "MA", lat: "42.25", lon: "-71.80" },
-    Michigan: { abbr: "MI", lat: "44.34", lon: "-85.41" },
-    Minnesota: { abbr: "MN", lat: "46.28", lon: "-94.30" },
-    Mississippi: { abbr: "MS", lat: "32.73", lon: "-89.66" },
-    Missouri: { abbr: "MO", lat: "38.35", lon: "-92.45" },
-    Montana: { abbr: "MT", lat: "47.05", lon: "-109.63" },
-    Nebraska: { abbr: "NE", lat: "41.53", lon: "-99.79" },
-    Nevada: { abbr: "NV", lat: "39.32", lon: "-116.63" },
-    New_Hampshire: { abbr: "NH", lat: "43.68", lon: "-71.58" },
-    New_York: { abbr: "NY", lat: "42.95", lon: "-75.52" },
-    North_Carolina: { abbr: "NC", lat: "35.55", lon: "-79.38" },
-    North_Dakota: { abbr: "ND", lat: "47.45", lon: "-100.46" },
-    Ohio: { abbr: "OH", lat: "40.28", lon: "-82.79" },
-    Oklahoma: { abbr: "OK", lat: "35.58", lon: "-97.49" },
-    Oregon: { abbr: "OR", lat: "43.93", lon: "-120.55" },
-    Pennsylvania: { abbr: "PA", lat: "40.87", lon: "-77.79" },
-    Rhode_Island: { abbr: "RI", lat: "41.67", lon: "-71.55" },
-    South_Carolina: { abbr: "SC", lat: "33.91", lon: "-80.89" },
-    South_Dakota: { abbr: "SD", lat: "44.44", lon: "-100.22" },
-    Tennessee: { abbr: "TN", lat: "35.85", lon: "-86.35" },
-    Texas: { abbr: "TX", lat: "31.47", lon: "-99.33" },
-    Utah: { abbr: "UT", lat: "39.30", lon: "-111.67" },
-    Vermont: { abbr: "VT", lat: "44.06", lon: "-72.66" },
-    Virginia: { abbr: "VA", lat: "37.52", lon: "-78.85" },
-    Washington: { abbr: "WA", lat: "47.38", lon: "-120.44" },
-    West_Virginia: { abbr: "WV", lat: "38.64", lon: "-80.62" },
-    Wisconsin: { abbr: "WI", lat: "44.62", lon: "-89.99" },
-    Wyoming: { abbr: "WY", lat: "42.99", lon: "-107.55" },
+    Alabama: { abbr: "al", lat: "32.77", lon: "-86.83" },
+    Alaska: { abbr: "ak", lat: "64.07", lon: "-152.28" },
+    Arizona: { abbr: "az", lat: "34.27", lon: "-111.66" },
+    Arkansas: { abbr: "ar", lat: "34.89", lon: "-92.44" },
+    California: { abbr: "ca", lat: "37.18", lon: "-119.47" },
+    Colorado: { abbr: "co", lat: "38.99", lon: "-105.55" },
+    Connencticut: { abbr: "ct", lat: "41.62", lon: "-105.55" },
+    Delaware: { abbr: "de", lat: "38.99", lon: "-75.51" },
+    DC: { abbr: "dc", lat: "38.91", lon: "-77.01" },
+    Florida: { abbr: "fl", lat: "28.68", lon: "-82.46" },
+    Georgia: { abbr: "ga", lat: "32.66", lon: "-83.43" },
+    Hawaii: { abbr: "hi", lat: "20.29", lon: "-156.37" },
+    Idaho: { abbr: "id", lat: "44.35", lon: "-114.61" },
+    Illinois: { abbr: "il", lat: "40.04", lon: "-89.19" },
+    Indiana: { abbr: "in", lat: "39.89", lon: "-86.28" },
+    Iowa: { abbr: "ia", lat: "42.07", lon: "-93.49" },
+    Kansas: { abbr: "ks", lat: "38.49", lon: "-98.38" },
+    Kentucky: { abbr: "ky", lat: "37.53", lon: "-85.30" },
+    Louisiana: { abbr: "la", lat: "31.06", lon: "-91.99" },
+    Maine: { abbr: "me", lat: "45.36", lon: "-69.24" },
+    Maryland: { abbr: "md", lat: "39.05", lon: "-76.79" },
+    Massachusetts: { abbr: "ma", lat: "42.25", lon: "-71.80" },
+    Michigan: { abbr: "mi", lat: "44.34", lon: "-85.41" },
+    Minnesota: { abbr: "mn", lat: "46.28", lon: "-94.30" },
+    Mississippi: { abbr: "ms", lat: "32.73", lon: "-89.66" },
+    Missouri: { abbr: "mo", lat: "38.35", lon: "-92.45" },
+    Montana: { abbr: "mt", lat: "47.05", lon: "-109.63" },
+    Nebraska: { abbr: "ne", lat: "41.53", lon: "-99.79" },
+    Nevada: { abbr: "nv", lat: "39.32", lon: "-116.63" },
+    New_Hampshire: { abbr: "nh", lat: "43.68", lon: "-71.58" },
+    New_York: { abbr: "ny", lat: "42.95", lon: "-75.52" },
+    North_Carolina: { abbr: "nc", lat: "35.55", lon: "-79.38" },
+    North_Dakota: { abbr: "nd", lat: "47.45", lon: "-100.46" },
+    Ohio: { abbr: "oh", lat: "40.28", lon: "-82.79" },
+    Oklahoma: { abbr: "ok", lat: "35.58", lon: "-97.49" },
+    Oregon: { abbr: "or", lat: "43.93", lon: "-120.55" },
+    Pennsylvania: { abbr: "pa", lat: "40.87", lon: "-77.79" },
+    Rhode_Island: { abbr: "ri", lat: "41.67", lon: "-71.55" },
+    South_Carolina: { abbr: "sc", lat: "33.91", lon: "-80.89" },
+    South_Dakota: { abbr: "sd", lat: "44.44", lon: "-100.22" },
+    Tennessee: { abbr: "tn", lat: "35.85", lon: "-86.35" },
+    Texas: { abbr: "tx", lat: "31.47", lon: "-99.33" },
+    Utah: { abbr: "ut", lat: "39.30", lon: "-111.67" },
+    Vermont: { abbr: "vt", lat: "44.06", lon: "-72.66" },
+    Virginia: { abbr: "va", lat: "37.52", lon: "-78.85" },
+    Washington: { abbr: "wa", lat: "47.38", lon: "-120.44" },
+    West_Virginia: { abbr: "wv", lat: "38.64", lon: "-80.62" },
+    Wisconsin: { abbr: "wi", lat: "44.62", lon: "-89.99" },
+    Wyoming: { abbr: "wy", lat: "42.99", lon: "-107.55" },
     // : {abbr: "", lat: "", lon: ""},
     // https://en.wikipedia.org/wiki/List_of_geographic_centers_of_the_United_States#Updated_list_of_geographic_centers
 
 }
-
-var currentTrip = {
-    location: "New-York",
-    startDate: "20200202",
-    endDate: "20200205"
-};
-
-var searchHistory = [{
-    location: "Texas",
-    startDate: "20201123",
-    endDate: "20201125"
-}];
 
 var getCurrentTrip = function() {
     if (JSON.parse(localStorage.getItem("currentTrip"))) {
@@ -79,7 +68,7 @@ var getCurrentTrip = function() {
         console.log("local storage current city was empty")
     }
 };
-//set current trip
+
 var storeCurrentTrip = function() {
     localStorage.setItem("currentTrip", JSON.stringify(currentTrip));
 };
@@ -92,7 +81,7 @@ var getHistory = function() {
     }
 };
 
-var setHistory = function() {
+var storeHistory = function() {
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 };
 
@@ -175,7 +164,7 @@ var map = new mapboxgl.Map({
         var date = moment().format("YYYYMMDD");
         //api call for covid data
         $.ajax({
-            url: `https://api.covidtracking.com/v1/states/${currentTrip.location}/${date}.json`,
+            url: `https://api.covidtracking.com/v1/states/${stateDatabank[currentTrip.location].abbr}/${date}.json`,
             method: "GET"
         }).then(function(response) {
             console.log(response);
@@ -193,7 +182,7 @@ var map = new mapboxgl.Map({
         var date = currentTrip.startDate;
         //api call for covid data
         $.ajax({
-            url: `https://api.covidtracking.com/v1/states/${currentTrip.location}/${date}.json`,
+            url: `https://api.covidtracking.com/v1/states/${stateDatabank[currentTrip.location].abbr}/${date}.json`,
             method: "GET"
         }).then(function(response) {
             console.log(response);
@@ -208,9 +197,6 @@ var map = new mapboxgl.Map({
             alert("Your ajax call failed.")
         })
     }
-
-    //api call for map
-
 };
 
 var renderButtons = function() {
@@ -218,6 +204,7 @@ var renderButtons = function() {
         var newButton = $('<div>')
         newButton.html(`
         <button class="button is-fullwidth" id="historyButton${i}">${searchHistory[i].location}</button>`)
+        $('#historyBox').append(newButton);
     }
 }
 
@@ -231,38 +218,46 @@ $(document).ready(function() {
     //load saved data
     getCurrentTrip();
 
-    //just testing
-    var currentTrip = {
-        location: "Texas",
-        startDate: "20200802",
-        endDate: "20200805",
-
-    }
     getHistory();
 
     //build current page
-    renderTrip(currentTrip);
+    if (currentTrip) {
+        renderTrip(currentTrip);
+    } else {
+        console.log("currentTrip was false")
+    }
+    
+    if (searchHistory) {
+        renderButtons();
+    } else {
+        console.log("searchHistory was false")
+    }
 
     $('#searchForm').submit(function(event) {
             event.preventDefault();
             //swap any spaces " " with underscore in state names
             //format start and end dates from MM/DD/YYYY to YYYYMMDD
-            var startDateInput = $('#startDateInput').val().replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3$1$2");
-            var endDateInput = $('#endDateInput').val().replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3$1$2");;
+            var startDateInput = $('#startDateInput').val();
+            var endDateInput = $('#endDateInput').val();
             var stateInput = $('#stateSearch').val().replace(' ', '_');
-            var historyButton = document.createElement("button");
-            historyButton.innerHTML = stateInput
-            document.getElementById('historyBox').appendChild(historyButton)
+            var formattedStart = moment(startDateInput, "MM/DD/YYYY").format("YYYYMMDD");
+            var formattedEnd = moment(endDateInput, "MM/DD/YYYY").format("YYYYMMDD");
+
+            currentTrip = {location: stateInput, startDate: formattedStart, endDate: formattedEnd};
+            if (searchHistory) {
+                searchHistory.push(currentTrip);
+            } else {
+                searchHistory = [currentTrip];
+            }
+            storeCurrentTrip();
+            storeHistory();
+            console.log(currentTrip);
+            console.log(searchHistory);
+            renderTrip();
+            renderButtons();
         })
         // stringify and parse current trip input
-    let currentTripObject = JSON.stringify(currentTrip);
-
-    localStorage.setItem("currentTripObject", currentTripObject);
-
-    let currentTripParse = JSON.parse(localStorage.getItem("currentTripObject"));
-
-    console.log(currentTripParse)
-
+    
     //store to currentTrip
     //setcurrenttrip
     //pushcurrenttrip to history object
